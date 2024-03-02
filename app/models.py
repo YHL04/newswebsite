2fb6942e09e140edea9318e1e5eda11223292218
Yi Hung Lim <yhlim99@outlook.com>
2024-03-02 10:43:27 -0500

    remote sql database

--- app/models.py
+++ app/models.py
@@ -4,16 +4,16 @@ from django.db import models
 # Create your models here.
 
 class News(models.Model):
-    id = models.CharField(max_length=4294967295, primary_key=True)
-    title = models.CharField(max_length=4294967295)
-    date = models.CharField(max_length=4294967295)
-    authors = models.CharField(max_length=4294967295)
-    categories = models.CharField(max_length=4294967295)
-    link = models.CharField(max_length=4294967295)
-    text = models.CharField(max_length=4294967295)
-    citation_rank = models.CharField(max_length=4294967295)
-    rank = models.CharField(max_length=4294967295)
-    likes = models.CharField(max_length=4294967295)
+    id = models.CharField(max_length=1_000_000, primary_key=True)
+    title = models.CharField(max_length=1_000_000)
+    date = models.CharField(max_length=1_000_000)
+    authors = models.CharField(max_length=1_000_000)
+    categories = models.CharField(max_length=1_000_000)
+    link = models.CharField(max_length=1_000_000)
+    text = models.CharField(max_length=1_000_000)
+    citation_rank = models.CharField(max_length=1_000_000)
+    rank = models.CharField(max_length=1_000_000)
+    likes = models.CharField(max_length=1_000_000)
 
     class Meta:
        managed = False
