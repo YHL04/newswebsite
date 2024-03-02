76984acba335c40c4735969db344592e1c50369d
Yi Hung Lim <yhlim99@outlook.com>
2024-03-02 11:23:50 -0500

    remote sql database

--- app/models.py
+++ app/models.py
@@ -4,16 +4,16 @@ from django.db import models
 # Create your models here.
 
 class News(models.Model):
-    id = models.CharField(max_length=1_000_000, unique=False, primary_key=True)
-    title = models.CharField(max_length=1_000_000, unique=False)
-    date = models.CharField(max_length=1_000_000, unique=False)
-    authors = models.CharField(max_length=1_000_000, unique=False)
-    categories = models.CharField(max_length=1_000_000, unique=False)
-    link = models.CharField(max_length=1_000_000, unique=False)
-    text = models.CharField(max_length=1_000_000, unique=False)
-    citation_rank = models.CharField(max_length=1_000_000, unique=False)
-    rank = models.CharField(max_length=1_000_000, unique=False)
-    likes = models.CharField(max_length=1_000_000, unique=False)
+    id = models.CharField(max_length=200, unique=False, primary_key=True)
+    title = models.CharField(max_length=200, unique=False)
+    date = models.CharField(max_length=200, unique=False)
+    authors = models.CharField(max_length=200, unique=False)
+    categories = models.CharField(max_length=200, unique=False)
+    link = models.CharField(max_length=200, unique=False)
+    text = models.CharField(max_length=200, unique=False)
+    citation_rank = models.CharField(max_length=200, unique=False)
+    rank = models.CharField(max_length=200, unique=False)
+    likes = models.CharField(max_length=200, unique=False)
 
     class Meta:
        managed = False
