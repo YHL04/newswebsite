dff2ce13b9ec48a2d9e977dcb07f27fd75b83fc9
Yi Hung Lim <yhlim99@outlook.com>
2024-03-03 20:46:14 -0500

    multithreaded ranker

--- run.py
+++ run.py
@@ -1,14 +1,11 @@
 
 
-import argparse
+import threading
 
 from backend import scraper, scraper_recent, ranker, reinit_db, store_to_db, get_from_db, delete_from_db
 
 
-if __name__ == "__main__":
-    keywords = ["Artificial Intelligence", "Machine Learning"]
-    categories = ["cs.AI", "cs.LG"]
-
+def scrape(keywords, categories):
     # scrape from arXiv
     for category in categories:
         print("Scraping %s" % category)
@@ -27,8 +24,8 @@ if __name__ == "__main__":
         except Exception as e:
             print(e)
 
-    # rank news from database
-    datas = get_from_db()
+
+def rank(datas, new_datas):
     datas = [datas[i:i+5] for i in range(0, len(datas), 5)]
 
     count = 0
@@ -37,8 +34,33 @@ if __name__ == "__main__":
         try:
             count += 5
             data = ranker(data)
-            delete_from_db(data)
-            store_to_db(data)
+            new_datas.extend(data)
         except Exception as e:
             print(e)
+
+
+if __name__ == "__main__":
+    keywords = ["Artificial Intelligence", "Machine Learning"]
+    categories = ["cs.AI", "cs.LG"]
+
+    # scrape(keywords, categories)
+
+    # rank news from database
+    datas = get_from_db()
+    datas = [datas[i:i+20] for i in range(0, len(datas), 20)]
+
+    i = 0
+    while i < len(datas):
+        new_data = []
+        threads = [threading.Thread(target=rank, args=(data, new_data)) for data in datas[i:i+10]]
+        for t in threads:
+            t.start()
+
+        for t in threads:
+            t.join()
+
+        delete_from_db(new_data)
+        store_to_db(new_data)
+
+        i += 10
 
