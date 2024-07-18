a2e7aec21bc0c609d5b00ec39340ca051e391594
Yi Hung Lim <yhlim99@outlook.com>
2024-07-18 15:58:33 -0400

    arxiv search

--- app/scraper.py
+++ app/scraper.py
@@ -1,13 +1,10 @@
 
 
 import arxiv
-import sshtunnel
-import MySQLdb
-import sqlite3
-from auth import *
 
 
 client = arxiv.Client()
+
 
 """
 Search arXiv in real time and store into database before displaying it to users so that users can like it
@@ -67,48 +64,5 @@ def arxiv_scraper(query, max_results):
 
         results.append(news)
 
-    store_to_db(results)
     return results
-
-
-def store_to_db(data):
-    with sshtunnel.SSHTunnelForwarder(
-        SSHHOST,
-        ssh_username=USERNAME,
-        ssh_password=PASSWORD,
-        remote_bind_address=SQLADDRESS
-    ) as tunnel:
-        con = MySQLdb.connect(
-            user=USERNAME,
-            passwd=SQLPASSWORD,
-            host=LOCALHOST,
-            port=tunnel.local_bind_port,
-            db=DBNAME,
-        )
-        data = [(str(d['id']), str(d["title"]), str(d["date"]), str(d["authors"]), str(d["categories"]),
-                 str(d["link"]), str(d["text"]), str(d["affiliations"]), str(d["citation_rank"]), str(d["final_rank"]),
-                 str(d["like_count"]))
-                for d in data]
-
-        cur = con.cursor()
-        cur.executemany(
-            "INSERT IGNORE INTO app_news VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-            data)
-        con.commit()
-        con.close()
-
-
-# def store_to_db(data, file="db.sqlite"):
-#     data = [(str(d['id']), str(d["title"]), str(d["date"]), str(d["authors"]), str(d["categories"]),
-#              str(d["link"]), str(d["text"]), str(d["affiliations"]), str(d["citation_rank"]), str(d["final_rank"]),
-#              str(d["like_count"]))
-#             for d in data]
-#
-#     with sqlite3.connect(file) as con:
-#         cur = con.cursor()
-#         cur.executemany(
-#             "INSERT INTO app_news VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-#             "ON CONFLICT DO NOTHING",
-#             data)
-#         con.commit()
 
